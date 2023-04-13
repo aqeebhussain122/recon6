f1a6b278dba202f21e949bd0b24ce4d2e6b84546
aqeebhussain122 <aqeebrajhussain@gmail.com>
2023-04-13 03:02:37 +0100

    Re-working with PyShark.

--- read_pcap_ipv6.py
+++ read_pcap_ipv6.py
@@ -24,60 +24,7 @@ Print out info about each packet in the pcap file
 
 # Processing of a given PCAP file to present detected link local IPv6 addresses
 def process_pcap(pcap):
-  packets = 0
-  time_list = []
-  mac_list = []
-  ip_details = []
-  # with open the csv file, we can just write the entire loop into a csv...
-  for timestamp, buf in pcap:
-      packets += 1
-      # Ethernet variable to assign a buffer to the PCAP file
-      eth = dpkt.ethernet.Ethernet(buf)
-      # Get all MAC addresses
-      mac_addresses = mac_addr(eth.dst)
-      mac_list.append(mac_addresses)
-      ip = eth.data
-
-      # If a packet is not ethernet+IP then discard it from the output, because we don't care about those.
-      if not isinstance(eth.data, dpkt.ip.IP):
-        pass
-        continue
-    
-      # Ignore IPv4
-      if isinstance(eth.data, dpkt.ip.IP):
-        pass
-        continue
-      
-#      elif isinstance(eth.data, dpkt.ip6.IP6):
-      #elif eth.type == dpkt.ethernet.ETH_TYPE_IP6:
-      if dpkt.ip.IP_PROTO_ICMP6 or dpkt.ip.IP_PROTO_IP6:
-        print(socket.inet_ntop(socket.AF_INET6, ip.src))
-        #connection = {}
-       # connection["src"] = socket.inet_ntop(socket.AF_INET6, ip.src)
-        #connection["dst"] = socket.inet_ntop(socket.AF_INET6, ip.dst)
-        #print(connection)
-
-	  # Seems like IPv4 which we probs do not want.
-      ip_src_str = socket.inet_ntoa(ip.src)
-      ip_dst_str = socket.inet_ntoa(ip.dst)
-
-      proto = ip.get_proto(ip.p).__name__
-      # We want to append a MAC address, the corresponding IPv4 address and source IPv6 address
-
-      date = str(datetime.utcfromtimestamp(timestamp).strftime("%Y-%m-%d"))
-      time = str(datetime.utcfromtimestamp(timestamp).strftime("%H:%M:%S"))
-      mins = str(datetime.utcfromtimestamp(timestamp).strftime("%M:%S.%f"))
-
-      # Using datetime objects we do calculations.
-      new_timestamp = str(datetime.utcfromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S.%f"))
-      test = datetime.strptime(new_timestamp, "%Y-%m-%d %H:%M:%S.%f")
-	  
-      time_list.append(test)
-      ip_details.append([proto, ip_src_str, ip_dst_str, mac_addr(eth.dst)])
-  
-  #print("MACs: {}".format(set(mac_list)))
-  #print("IP Details: {}".format(ip_details))
-
+# Rework on this with Pyshark.
 
 def main():
   # Open a binary file stream of the pcap file
