5a50980efdfb0090de404434c5e124acce4aab40
aqeebhussain122 <aqeebrajhussain@gmail.com>
2023-04-14 03:37:52 +0100

    IPv4 dictionary made.

--- read_pcap.py
+++ read_pcap.py
@@ -23,35 +23,38 @@ Print out info about each packet in the pcap file
 
 # Processing of a given PCAP file to present detected link local IPv6 addresses
 def process_pcap(pcap):
+    # We use JSON to help us know the field names of the fields to print for our tools
     read_pcap = pyshark.FileCapture(pcap, use_json=True)
-    # Dictionary to get an IPv6 addresses and the MAC address.
+    # Dictionary to get an IPv6 addresses and the MAC address via NDP.
     ipv6_pkts = {}
-    # Dictionary to store the IPv4 address and the corresponding MAC.
+    # Dictionary to store the IPv4 address and the corresponding MAC via ARP.
     ipv4_pkts = {}
 
     for pkt in read_pcap:
-        # Look for all packets which are of a TCP origin before digging into the IP layer.
+        # Look for all packets which are of a TCP origin and checking IPv6 is also part of the layer(s).
         if "TCP" and "IPv6" in pkt:
             # Print the IPv6 details of the packet as a whole.
             #print(pkt.ipv6)
+            
             # If we want the source IPv6 address which is what we'll want because the destination is a multicast call to all nodes.
             src_ipv6 = pkt.ipv6.src # We want to store this in a variable and then store it in our dictionary.
             src_ndp_mac = pkt.eth.src  # We want to store this in a variable and then store it in our dictionary.
             ipv6_pkts['{}'.format(src_ipv6)] = src_ndp_mac
             # Corresponding MAC which is retrieved via NDP
             # Add all of the MACs which have been found
-            #pkt_macs.append(pkt.eth.src)
 
         # We attempt to find our MAC address via packets where ARP is present.
         if "arp" in pkt:
-            pass
-            src_ipv4 = pkt[1].src.proto.ipv4
-            src_arp_mac = 
+            src_ipv4 = pkt[1].src.proto_ipv4
+            src_arp_mac = pkt[1].src.hw_mac
+            ipv4_pkts['{}'.format(src_ipv4)] = src_arp_mac
             #print(pkt[1].src.proto_ipv4)
             #print(pkt[1].src.hw_mac)
 
     
+    # Print the dictionaries we need.
     print(ipv6_pkts)
+    print(ipv4_pkts)
     # All of the MAC addresses collected. 
     #for i in range(len(pkt_macs)):
         #print(pkt_macs[i])
